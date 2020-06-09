99c951b4d09d5d5513ad8a3c093dee606f2ea5b8
saji <saji@2kyu.moe>
2020-06-08 21:02:46 -0500

    fix main.py to work with new API spec.

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 import astrolightinterop.RAPID.rapidmodel as rapidmodel
 import logging
 import matplotlib.pyplot as plt
-
+import pandas as pd
 logging.basicConfig(level=logging.INFO)
-model = rapidmodel.RAPIDModel('data/training_set.csv', 'data/training_set_metadata.csv')
+
+training_data = pd.read_csv('data/training_set.csv')
+training_meta = pd.read_csv('data/training_set_metadata.csv')
+model = rapidmodel.RAPIDModel(training_data, training_meta)
 targets, predictions = model.test()
