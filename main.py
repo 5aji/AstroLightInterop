a38cadf1dc79894451721a128ddc9c12a65f79c9
saji <saji@2kyu.moe>
2020-06-08 21:28:25 -0500

    fix index issue with main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 logging.basicConfig(level=logging.INFO)
 
-training_data = pd.read_csv('data/training_set.csv')
-training_meta = pd.read_csv('data/training_set_metadata.csv')
+training_data = pd.read_csv('data/training_set.csv', index_col=['object_id', 'mjd'])
+training_meta = pd.read_csv('data/training_set_metadata.csv', index_col='object_id')
 model = rapidmodel.RAPIDModel(training_data, training_meta)
 targets, predictions = model.test()
